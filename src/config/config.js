a7c534fbb7f44740ef0539cbd1806afab4ef92f2
juancarlos0330 <juancarlos.king0330@gmail.com>
2024-10-29 16:17:08 +0900

    - Update the config setting

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const apiURL = "http://85.208.110.183:5000/api";
-export const socketURL = "http://85.208.110.183:5000";
+export const apiURL = "http://localhost:5000/api";
+export const socketURL = "http://localhost:5000";
